4346417c67df4d65542285f0d78cf061617f8ca9
VladimirBelyaev <vogisa@mail.ru>
2025-04-14 02:31:00 +0300

    added axios pinia and vue-router to package.json

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,3 @@
 <template>
-	<RouterView />
+	<h1>Home page</h1>
 </template>
---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createWebHistory, createRouter } from 'vue-router'
+
+const routes = [
+	{
+		path: '/',
+		component: () => import('@/pages/HomePage.vue')
+	},
+	// {
+	// 	path: '/:pathMatch(.*)*',
+	// 	component: () => import('@/layouts/404.vue'),
+	// },
+]
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+})
+
+export default router
