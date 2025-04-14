dbc146759426b9fc19954f09c5206a44e8071fa3
VladimirBelyaev <vogisa@mail.ru>
2025-04-14 03:14:01 +0300

    added 404 layout

---
+++ src/layouts/404.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="flex flex-col gap-4 justify-center items-center w-full" :style="{ height: '100vh' }">
+		<h1>404 | Not Found</h1>
+		<NButton @click="routeToProductsPage">Go to products page</NButton>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+import { NButton } from 'naive-ui'
+
+const router = useRouter()
+
+const routeToProductsPage = () => router.push('/products')
+</script>
